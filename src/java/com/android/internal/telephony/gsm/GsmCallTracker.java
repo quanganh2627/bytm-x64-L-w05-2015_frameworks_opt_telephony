ca2856e1644c1c0c1ef61be47ffd36d7f8ebea8a
Arun Ravindran <arun.ravindran@intel.com>
2012-11-19 16:42:28 +0200

    Telephony: SetAudioMode to IN_CALL on incoming call accept
    BZ: 31518
    
    [DO NOT UPSTREAM]
    
    According to 3GPP conformance test cases 26.9.4 or
    26.9.5 or 26.19.3a, audio has to be available
    when CONNECT is sent to NW in a MT call case.
    
    Audio path opening sequence is triggered by
    phone application. Phone application calls
    the SetAudioMode on phone state changes and also
    after each user operation. But as per telephony
    framework state, call is still in RINGING state even
    if the user has accepted the call resulting in
    setMode called with AudioManager.MODE_RINGTONE.
    So, audio path opening is blocked due to the missing
    setMode with AudioManager.MODE_IN_CALL.
    
    Fix: Incoming call accepted by user information will be
    stored as part of the call object and will checked before
    setting the audio mode. If the call is ringing but accepted
    by user setMode will be called with AudioManager.MODE_IN_CALL or
    else setMode will be called with AudioManager.MODE_RINGTONE.
    Thi flag will be cleared on call disconnected. This way
    changes/fixes will be internal to the framework.
    
    Change-Id: I5c68f9899bb07fca2f8a4c1dce8ad03b8f0998c4
    Orig-Change-Id: Ibc5cb999142fe6ddadb01f065c5f3be0eff16263
    Orig-Change-Id: If72751b4616961ac34bb3c120e684814429d6af3
    Signed-off-by: Jeevaka Badrappan <jeevaka.badrappan@intel.com>
    Signed-off-by: Arun Ravindran <arun.ravindran@intel.com>
    Reviewed-on: http://android.intel.com:8080/75739
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Reviewed-by: Fremaux, Pascal <pascal.fremaux@intel.com>
    Reviewed-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Tested-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- src/java/com/android/internal/telephony/gsm/GsmCallTracker.java
+++ src/java/com/android/internal/telephony/gsm/GsmCallTracker.java
@@ -252,6 +252,7 @@ public final class GsmCallTracker extends CallTracker {
             Log.i("phone", "acceptCall: incoming...");
             // Always unmute when answering a new call
             setMute(false);
+            ringingCall.setIncomingAcceptedByUser(true);
             cm.acceptCall(obtainCompleteMessage());
         } else if (ringingCall.getState() == GsmCall.State.WAITING) {
             setMute(false);
