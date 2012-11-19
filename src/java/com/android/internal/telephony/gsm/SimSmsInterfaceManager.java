d68bab3a96fb40ea90b8c83fb73b1da1ae885854
Arun Ravindran <arun.ravindran@intel.com>
2012-11-19 15:12:14 +0200

    Telephony: Fix issue in SMS-CB range management
    BZ: 8118
    
    Android SMS-CB range management cannot manage several ranges.
    This patch fixes this issue.
    
    Change-Id: I73bc30177fc8786d2bec7322e2330991b4ffc154
    Orig-Change-Id: Id240252753a2814bbac08f7fac625d0cc60bd623
    Signed-off-by: Emmanuel Delaude <emmanuelx.delaude@intel.com>
    Signed-off-by: Arun Ravindran <arun.ravindran@intel.com>
    Reviewed-on: http://android.intel.com:8080/75818
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Reviewed-by: Badrappan, Jeevaka <jeevaka.badrappan@intel.com>
    Reviewed-by: Fremaux, Pascal <pascal.fremaux@intel.com>
    Reviewed-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Tested-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- src/java/com/android/internal/telephony/gsm/SimSmsInterfaceManager.java
+++ src/java/com/android/internal/telephony/gsm/SimSmsInterfaceManager.java
@@ -292,14 +292,27 @@ public class SimSmsInterfaceManager extends IccSmsInterfaceManager {
         }
 
         /**
-         * Called after {@link #startUpdate} to indicate a range of enabled
-         * values.
+         * Called after {@link #startUpdate} to add range(s) of enabled
+         * values to the config list.
          * @param startId the first id included in the range
          * @param endId the last id included in the range
          */
         protected void addRange(int startId, int endId, boolean selected) {
-            mConfigList.add(new SmsBroadcastConfigInfo(startId, endId,
-                        SMS_CB_CODE_SCHEME_MIN, SMS_CB_CODE_SCHEME_MAX, selected));
+            boolean found = false;
+            if (!mConfigList.isEmpty()) {
+                for (int i = 0; i < mConfigList.size() ; i++) {
+                    if ((mConfigList.get(i).getFromServiceId() == startId)
+                            && (mConfigList.get(i).getToServiceId() == endId)) {
+                        mConfigList.get(i).setSelected(selected);
+                        found = true;
+                    }
+                }
+            }
+            if (!found) {
+                mConfigList.add(new SmsBroadcastConfigInfo(startId, endId,
+                            SMS_CB_CODE_SCHEME_MIN, SMS_CB_CODE_SCHEME_MAX,
+                            selected));
+            }
         }
 
         /**
