quanganh2627/bytm-x64-L-w05-2015_frameworks_opt_telephony

commit d841c480433fc980f826ecc5a427c2a5a18c515a

telephony: Add support for SET UP EVENT LIST
BZ: 70855

This patch includes the following changes:

- Adds the support for handling of
  SET UP EVENT LIST proactive command.
- Adds onEventDownload callback function
  for STK app to pass the event to CAT.
- Adds isEventDownloadActive function
  for STK app to check whether an event
  download is active for the given
  event.

Change-Id: I91b0b25a14f25b4db3b40c8e5fc9a981a7a96c96
Signed-off-by: Guillaume Lucas <guillaume.lucas@intel.com>
Signed-off-by: Jeevaka Badrappan <jeevaka.badrappan@intel.com>
Reviewed-on: http://android.intel.com:8080/76843
Reviewed-by: Lebsir, SamiX <samix.lebsir@intel.com>
Tested-by: Lebsir, SamiX <samix.lebsir@intel.com>
Reviewed-by: Roques, CyrilX <cyrilx.roques@intel.com>
Reviewed-by: Fremaux, Pascal <pascal.fremaux@intel.com>
Reviewed-by: cactus <cactus@intel.com>
Tested-by: cactus <cactus@intel.com>

src/java/com/android/internal/telephony/cat/CatService.java
@@ -76,6 +76,8 @@ public class CatService extends Handler implements AppInterface {
     private CatCmdMessage mCurrntCmd = null;
     private CatCmdMessage mMenuCmd = null;
 
+    private byte[] mEventList = null;
+
     private RilMessageDecoder mMsgDecoder = null;
     private boolean mStkAppInstalled = false;
 
@@ -93,12 +95,12 @@ public class CatService extends Handler implements AppInterface {
     // Events to signal SIM presence or absent in the device.
     private static final int MSG_ID_ICC_RECORDS_LOADED       = 20;
 
-    private static final int DEV_ID_KEYPAD      = 0x01;
-    private static final int DEV_ID_DISPLAY     = 0x02;
-    private static final int DEV_ID_EARPIECE    = 0x03;
-    private static final int DEV_ID_UICC        = 0x81;
-    private static final int DEV_ID_TERMINAL    = 0x82;
-    private static final int DEV_ID_NETWORK     = 0x83;
+    public static final int DEV_ID_KEYPAD      = 0x01;
+    public static final int DEV_ID_DISPLAY     = 0x02;
+    public static final int DEV_ID_EARPIECE    = 0x03;
+    public static final int DEV_ID_UICC        = 0x81;
+    public static final int DEV_ID_TERMINAL    = 0x82;
+    public static final int DEV_ID_NETWORK     = 0x83;
 
     static final String STK_DEFAULT = "Default Message";
 
@@ -235,6 +237,11 @@ public class CatService extends Handler implements AppInterface {
                 }
                 break;
             case REFRESH:
+                // Invalidate event list if the SIM resets
+                if (cmdParams.cmdDet.commandQualifier
+                        == CommandParamsFactory.REFRESH_UICC_RESET) {
+                    mEventList = null;
+                }
                 // ME side only handles refresh commands which meant to remove IDLE
                 // MODE TEXT.
                 cmdParams.cmdDet.typeOfCommand = CommandType.SET_UP_IDLE_MODE_TEXT.value();
@@ -287,6 +294,20 @@ public class CatService extends Handler implements AppInterface {
                     message = mContext.getText(com.android.internal.R.string.SetupCallDefault);
                     ((CallSetupParams) cmdParams).confirmMsg.text = message.toString();
                 }
+                break;
+           case SET_UP_EVENT_LIST:
+                // Store eventlist
+                mCurrntCmd = cmdMsg;
+                mEventList = mCurrntCmd.getEventList();
+                if (mEventList != null) {
+                    for (byte b : mEventList) {
+                        CatLog.d(this, "Registered Event: " + b);
+                    }
+                } else {
+                    CatLog.d( this, "WARNING: No Event in event list!" );
+                }
+
+                sendTerminalResponse(cmdParams.cmdDet, ResultCode.OK, false, 0, null);
                 break;
             case OPEN_CHANNEL:
             case CLOSE_CHANNEL:
@@ -506,8 +527,52 @@ public class CatService extends Handler implements AppInterface {
         mCmdIf.sendEnvelope(hexString, null);
     }
 
+    public boolean isEventDownloadActive(int event) {
+        if (mEventList != null) {
+            for (byte b : mEventList) {
+                if (b == (byte)event) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public void updateEventList(byte event, byte value) {
+        if (mEventList != null) {
+            for (int i = 0; i < mEventList.length; i++) {
+                if (mEventList[i] == event) {
+                    mEventList[i] = value;
+                    break;
+                }
+            }
+        }
+    }
+
+    public void onEventDownload(CatEventMessage eventMsg) {
+        CatLog.d(this, "Download event: " + eventMsg.getEvent());
+        eventDownload(eventMsg.getEvent(),
+                eventMsg.getSourceId(),
+                eventMsg.getDestId(),
+                eventMsg.getAdditionalInfo(),
+                eventMsg.isOneShot());
+    }
+
     private void eventDownload(int event, int sourceId, int destinationId,
             byte[] additionalInfo, boolean oneShot) {
+
+        CatLog.d(this, "eventDownload()  "+ event+ "  " + sourceId + " ");
+        // Check if the SIM have subscribed to this event using setup eventlist
+        boolean allowed = isEventDownloadActive((byte)event);
+        if (!allowed) {
+            CatLog.d(this, "(U)SIM has not subscribed for event: " + event);
+            return;
+        }
+
+        if (oneShot) {
+            updateEventList((byte)event, (byte)EventCode.INVALID_EVENT.value());
+        }
 
         ByteArrayOutputStream buf = new ByteArrayOutputStream();
 
@@ -620,6 +685,7 @@ public class CatService extends Handler implements AppInterface {
     @Override
     public void handleMessage(Message msg) {
 
+        CatLog.d(this, "handleMessage() " + msg.what);
         switch (msg.what) {
         case MSG_ID_SESSION_END:
         case MSG_ID_PROACTIVE_COMMAND:
@@ -688,6 +754,11 @@ public class CatService extends Handler implements AppInterface {
     }
 
     private void handleCmdResponse(CatResponseMessage resMsg) {
+        if ((resMsg != null) && (resMsg.mEnvelopeCmd != null)) {
+            mCmdIf.sendEnvelope(resMsg.mEnvelopeCmd, null);
+            return;
+        }
+
         // Make sure the response details match the last valid command. An invalid
         // response is a one that doesn't have a corresponding proactive command
         // and sending it can "confuse" the baseband/ril.

src/java/com/android/internal/telephony/cat/AppInterface.java
@@ -39,6 +39,16 @@ public interface AppInterface {
     void onCmdResponse(CatResponseMessage resMsg);
 
     /*
+     * Returns true - Event download active for the given event.
+     */
+    boolean isEventDownloadActive(int event);
+
+    /*
+     * Callback function from app to telephony to pass a event to the SIM.
+     */
+    void onEventDownload(CatEventMessage eventMsg);
+
+    /*
      * Enumeration for representing "Type of Command" of proactive commands.
      * Those are the only commands which are supported by the Telephony. Any app
      * implementation should support those.

src/java/com/android/internal/telephony/cat/CatCmdMessage.java
@@ -33,6 +33,7 @@ public class CatCmdMessage implements Parcelable {
     private BrowserSettings mBrowserSettings = null;
     private ToneSettings mToneSettings = null;
     private CallSettings mCallSettings = null;
+    private byte[] mEventList = null;
 
     /*
      * Container for Launch Browser command settings.
@@ -53,6 +54,9 @@ public class CatCmdMessage implements Parcelable {
     CatCmdMessage(CommandParams cmdParams) {
         mCmdDet = cmdParams.cmdDet;
         switch(getCmdType()) {
+        case SET_UP_EVENT_LIST:
+            mEventList = ((EventListParams) cmdParams).eventList;
+            break;
         case SET_UP_MENU:
         case SELECT_ITEM:
             mMenu = ((SelectItemParams) cmdParams).menu;
@@ -101,6 +105,14 @@ public class CatCmdMessage implements Parcelable {
         mMenu = in.readParcelable(null);
         mInput = in.readParcelable(null);
         switch (getCmdType()) {
+        case SET_UP_EVENT_LIST:
+            mEventList = null;
+            int len = in.readInt();
+            if (len > 0) {
+                mEventList = new byte[len];
+                in.readByteArray(mEventList);
+            }
+            break;
         case LAUNCH_BROWSER:
             mBrowserSettings = new BrowserSettings();
             mBrowserSettings.url = in.readString();
@@ -123,6 +135,16 @@ public class CatCmdMessage implements Parcelable {
         dest.writeParcelable(mMenu, 0);
         dest.writeParcelable(mInput, 0);
         switch(getCmdType()) {
+        case SET_UP_EVENT_LIST:
+            int len = 0;
+            if (mEventList != null) {
+                len = mEventList.length;
+            }
+            dest.writeInt(len);
+            if (len > 0) {
+                dest.writeByteArray(mEventList);
+            }
+            break;
         case LAUNCH_BROWSER:
             dest.writeString(mBrowserSettings.url);
             dest.writeInt(mBrowserSettings.mode.ordinal());
@@ -154,6 +176,10 @@ public class CatCmdMessage implements Parcelable {
     /* external API to be used by application */
     public AppInterface.CommandType getCmdType() {
         return AppInterface.CommandType.fromInt(mCmdDet.typeOfCommand);
+    }
+
+    public byte[] getEventList() {
+        return mEventList;
     }
 
     public Menu getMenu() {

src/java/com/android/internal/telephony/cat/CatEventMessage.java
@@ -1,0 +1,58 @@
+/*
+ * Copyright (C) 2010 Giesecke & Devrient GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.telephony.cat;
+
+public class CatEventMessage {
+    private int mEvent = 0;
+    private int mSourceId = CatService.DEV_ID_TERMINAL;
+    private int mDestId = CatService.DEV_ID_UICC;
+    private byte[] mAdditionalInfo = null;
+    private boolean mOneShot = false;
+
+    public CatEventMessage(int event, int sourceId, int destId, byte[] additionalInfo,
+            boolean oneShot) {
+        this.mEvent = event;
+        this.mSourceId = sourceId;
+        this.mDestId = destId;
+        this.mAdditionalInfo = additionalInfo;
+        this.mOneShot = oneShot;
+    }
+
+    public CatEventMessage(int event, byte[] additionalInfo, boolean oneShot) {
+        this.mEvent = event;
+        this.mAdditionalInfo = additionalInfo;
+        this.mOneShot = oneShot;
+    }
+
+    public int getEvent() {
+        return mEvent;
+    }
+
+    public int getSourceId() {
+        return mSourceId;
+    }
+
+    public int getDestId() {
+        return mDestId;
+    }
+    public byte[] getAdditionalInfo() {
+        return mAdditionalInfo;
+    }
+    public boolean isOneShot() {
+        return mOneShot;
+    }
+}

src/java/com/android/internal/telephony/cat/CatResponseMessage.java
@@ -25,6 +25,12 @@ public class CatResponseMessage {
         boolean usersConfirm = false;
         boolean includeAdditionalInfo = false;
         int additionalInfo = 0;
+        String mEnvelopeCmd;
+
+        public CatResponseMessage(String envCmd) {
+            this.mEnvelopeCmd = envCmd;
+        }
+
         public CatResponseMessage(CatCmdMessage cmdMsg) {
             this.cmdDet = cmdMsg.mCmdDet;
         }

src/java/com/android/internal/telephony/cat/CommandParams.java
@@ -171,6 +171,14 @@ class GetInputParams extends CommandParams {
     }
 }
 
+class EventListParams extends CommandParams {
+    byte[] eventList = null;
+    EventListParams(CommandDetails cmdDet, byte[] eventList) {
+        super(cmdDet);
+        this.eventList = eventList;
+    }
+}
+
 /*
  * BIP (Bearer Independent Protocol) is the mechanism for SIM card applications
  * to access data connection through the mobile device.

src/java/com/android/internal/telephony/cat/CommandParamsFactory.java
@@ -126,6 +126,9 @@ class CommandParamsFactory extends Handler {
 
         try {
             switch (cmdType) {
+            case SET_UP_EVENT_LIST:
+                cmdPending = processSetUpEventList(cmdDet, ctlvs);
+                break;
             case SET_UP_MENU:
                 cmdPending = processSelectItem(cmdDet, ctlvs);
                 break;
@@ -667,18 +670,23 @@ class CommandParamsFactory extends Handler {
             List<ComprehensionTlv> ctlvs) {
 
         CatLog.d(this, "process SetUpEventList");
-        //
-        // ComprehensionTlv ctlv = searchForTag(ComprehensionTlvTag.EVENT_LIST,
-        // ctlvs);
-        // if (ctlv != null) {
-        // try {
-        // byte[] rawValue = ctlv.getRawValue();
-        // int valueIndex = ctlv.getValueIndex();
-        // int valueLen = ctlv.getLength();
-        //
-        // } catch (IndexOutOfBoundsException e) {}
-        // }
-        return true;
+        ComprehensionTlv ctlv = searchForTag(ComprehensionTlvTag.EVENT_LIST,ctlvs);
+        if (ctlv != null) {
+            byte[] rawValue = ctlv.getRawValue();
+            int valueIndex = ctlv.getValueIndex();
+            int valueLength = ctlv.getLength();
+            byte[] eventList;
+            if (valueLength > 0 && rawValue.length >= (valueIndex + valueLength)) {
+                eventList = new byte[valueLength];
+                if (eventList != null) {
+                    System.arraycopy(rawValue, valueIndex, eventList, 0, valueLength);
+                }
+            } else {
+                eventList = null;
+            }
+            mCmdParams = new EventListParams(cmdDet, eventList);
+        }
+        return false;
     }
 
     /**

src/java/com/android/internal/telephony/cat/EventCode.java
@@ -1,0 +1,64 @@
+/*
+ * Copyright (C) 2006 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.telephony.cat;
+/**
+ * Enumeration for the event code in SET UP EVENT LIST and event download.
+ * To get the actual return code for each enum value, call {@link #code() code}
+ * method.
+ *
+ * {@hide}
+ */
+public enum EventCode {
+    MT_CALL(0x00),
+    CALL_CONNECTED(0x01),
+    CALL_DISCONNECTED(0x02),
+    LOACATION_STATUS(0x03),
+    USER_ACTIVITY(0x04),
+    IDLE_SCREEN_AVAILABLE(0x05),
+    CARD_READER_STATUS(0x06),
+    LANGUAGE_SELECTION(0x07),
+    BROWSER_TERMINATION(0x08),
+    DATA_AVAILABLE(0x09),
+    CHANNEL_STATUS(0x0A),
+    NETWORK_SEARCH_MODE_CHANGE(0x0E),
+    HCI_CONNECTIVITY_EVENT(0x13),
+    INVALID_EVENT(0xFF);
+
+    private int mCode;
+
+    EventCode(int code) {
+       mCode = code;
+    }
+
+    /**
+     * Retrieves the actual result code that this object represents.
+     * @return Actual result code
+     */
+   public int value() {
+        return mCode;
+    }
+
+   public static EventCode fromInt(int value) {
+       for (EventCode r : EventCode.values()) {
+            if (r.mCode == value) {
+                return r;
+            }
+        }
+        return null;
+    }
+}
+

commit bd108e098c209a2ea184e461805a2efb974bf456

telephony: Fix critical KW errors
BZ: 85540

[DO NOT UPSTREAM]

Fixes critical KW errors introduced by commits
d841c480433fc980f826ecc5a427c2a5a18c515a
e8701beb449c1aa0febba4aa96b93c21ff0d6e8e
1d22d483cade2261ad978b02c47ca100a5ca7333

Change-Id: Icb10212312043aecbdb3b4e9c9115e05ed06c7eb
Signed-off-by: Jeevaka Badrappan <jeevaka.badrappan@intel.com>
Reviewed-on: http://android.intel.com:8080/90838
Reviewed-by: cactus <cactus@intel.com>
Reviewed-by: Robert, Denis <denis.robert@intel.com>
Reviewed-by: Yao, Yong <yong.yao@intel.com>
Reviewed-by: Ravindran, Arun <arun.ravindran@intel.com>
Reviewed-by: Lebsir, SamiX <samix.lebsir@intel.com>
Tested-by: Lebsir, SamiX <samix.lebsir@intel.com>
Reviewed-by: buildbot <buildbot@intel.com>
Tested-by: buildbot <buildbot@intel.com>

src/java/com/android/internal/telephony/cat/BipGateWay.java
@@ -1139,9 +1139,9 @@ public class BipGateWay {
                                new TrustManager[] {new SCWSTrustManager()},
                                null);
                         SSLSocket socket = (SSLSocket)sslContext.getSocketFactory().createSocket();
+                        mSocket = socket;
                         socket.connect(new InetSocketAddress(addr, mChannelSettings.port));
                         socket.startHandshake();
-                        mSocket = socket;
                     } else {
                         addr = InetAddress.getLocalHost();
                         mSocket = new Socket(addr, mChannelSettings.port);
@@ -1167,6 +1167,15 @@ public class BipGateWay {
                 } catch (Exception e) {
                     CatLog.d(this, "OPEN_CHANNEL - Client connection failed: "
                                 + e.getMessage() );
+                    if (mSocket != null) {
+                        if (!mSocket.isClosed()) {
+                            try {
+                                mSocket.close();
+                            } catch (IOException ioEx) {
+                            }
+                        }
+                    }
+
                     ResponseData resp = new OpenChannelResponseData(
                                 mChannelSettings.bufSize, mChannelStatus,
                                 mChannelSettings.bearerDescription);

src/java/com/android/internal/telephony/cat/CatService.java
@@ -813,7 +813,7 @@ public class CatService extends Handler implements AppInterface {
     }
 
     private boolean validateResponse(CatResponseMessage resMsg) {
-        if (mCurrntCmd != null) {
+        if (mCurrntCmd != null && resMsg != null) {
             return (resMsg.cmdDet.compareTo(mCurrntCmd.mCmdDet));
         }
         return false;

src/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
@@ -1382,8 +1382,10 @@ final class GsmServiceStateTracker extends ServiceStateTracker {
 
         boolean equalsMcc = true;
         try {
-            equalsMcc = simNumeric.substring(0, 3).
-                    equals(operatorNumeric.substring(0, 3));
+            if (operatorNumeric != null) {
+                equalsMcc = simNumeric.substring(0, 3).
+                        equals(operatorNumeric.substring(0, 3));
+            }
         } catch (Exception e){
         }
 