fd7f277447df707125d31cb32e7f8f729cdaafad
Madhava Rao <sethu.madhavax.rao@intel.com>
2013-03-21 12:36:52 +0100

    Phone: Selection of LTE/ WCDMA/ GSM from UI
    BZ: 94202
    
    To select the Preferred Network from UI:
    GSM/WCDMA Preferred
    GSM only
    WCDMA only
    LTE only
    LTE/GSM/WCDMA
    
    Change-Id: I202c4355590c1117f7f0ee19e7c6021158aa3aa0
    Signed-off-by: Madhava Rao <sethu.madhavax.rao@intel.com>
    Reviewed-on: http://android.intel.com:8080/97736
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Josso, Nicolas <nicolas.josso@intel.com>
    Reviewed-by: Lucas, Guillaume <guillaume.lucas@intel.com>
    Reviewed-by: Fremaux, Pascal <pascal.fremaux@intel.com>
    Reviewed-by: Chen, HuaqiangX <huaqiangx.chen@intel.com>
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Reviewed-by: Ravindran, Arun <arun.ravindran@intel.com>
    Reviewed-by: Marmounier, Edward <edward.marmounier@intel.com>
    Tested-by: Louis, NicolasX <nicolasx.louis@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- src/java/com/android/internal/telephony/Phone.java
+++ src/java/com/android/internal/telephony/Phone.java
@@ -129,6 +129,7 @@ public interface Phone {
     int NT_MODE_GLOBAL       = RILConstants.NETWORK_MODE_GLOBAL;
 
     int NT_MODE_LTE_ONLY     = RILConstants.NETWORK_MODE_LTE_ONLY;
+    int NT_MODE_LTE_GSM_WCDMA     = RILConstants.NETWORK_MODE_LTE_GSM_WCDMA;
     int PREFERRED_NT_MODE    = RILConstants.PREFERRED_NETWORK_MODE;
 
 
