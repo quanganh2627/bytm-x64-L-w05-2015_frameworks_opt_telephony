6d409e630a836e67cfb6293c0e93517f165e4804
Arun Ravindran <arun.ravindran@intel.com>
2013-04-30 16:09:33 +0300

    [PORT FROM MAIN] Telephony: Clear CallForwarding and Message waiting Indications
    BZ: 64610
    
    When SIM is removed the SIMRecords object is disposed, but
    the relevant events to clear the Callforwarding and message
    waiting icons are not send.
    
    This patch clears the call forwarding and message waiting
    indicators by clearing the internal member data and sending
    out notifications for the change in data.
    
    Category: AOSP Improvement
    Domain: Telephony-Telephony-FWK
    Origin: Internal
    Upstream-Candidate: yes
    
    Change-Id: I535b0aa1ba740cf44dc1442a9ddb3e98cf7e8912
    Orig-Change-Id: I9a123c8fbeccf2c6a11d1d60a58eeb9898f25cdb
    Signed-off-by: Arun Ravindran <arun.ravindran@intel.com>

--- src/java/com/android/internal/telephony/uicc/SIMRecords.java
+++ src/java/com/android/internal/telephony/uicc/SIMRecords.java
@@ -233,6 +233,10 @@ public class SIMRecords extends IccRecords {
         mSpn = null;
         mEfMWIS = null;
         mEfCPHS_MWI = null;
+        setVoiceMessageWaiting(1, 0);
+        mEfCfis = null;
+        mEfCff = null;
+        setVoiceCallForwardingFlag(1, false, null);
         mSpdiNetworks = null;
         mPnnHomeName = null;
         mGid1 = null;
