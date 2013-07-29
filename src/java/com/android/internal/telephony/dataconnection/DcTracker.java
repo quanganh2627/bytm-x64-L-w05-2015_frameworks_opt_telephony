e5c830b322685897d7d6502a113473aabe32ded9
Alain Kouassu <alain.kouassu@intel.com>
2013-07-29 13:02:44 +0300

    [PORT FROM MAIN] Telephony: Definition of new data profile based on apn type
    BZ: 32959
    
    Mapping  of new data profiles MMS, HIPRI and SUPL in framework.
    The value of the data profile is determined based on the the apn type
    define in Phone.java.
    
    Change-Id: Id953c260b1e1e71f11435c3f43235ee4fec4734f
    Orig-Change-Id: Ic9e96c6f2d82dc8e24a950d09303007039a8afd6
    Signed-off-by: Alain Kouassu <alain.kouassu@intel.com>
    Signed-off-by: Arun Ravindran <arun.ravindran@intel.com>
    Reviewed-on: http://android.intel.com:8080/121650
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Tested-by: Yao, Yong <yong.yao@intel.com>

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -2166,6 +2166,14 @@ public final class DcTracker extends DcTrackerBase {
             return RILConstants.DATA_PROFILE_FOTA;
         } else if (TextUtils.equals(apnType, PhoneConstants.APN_TYPE_CBS)) {
             return RILConstants.DATA_PROFILE_CBS;
+        } else if (TextUtils.equals(apnType, PhoneConstants.APN_TYPE_MMS)) {
+            return (RILConstants.DATA_PROFILE_MMS);
+        } else if (TextUtils.equals(apnType, PhoneConstants.APN_TYPE_DUN)) {
+            return RILConstants.DATA_PROFILE_TETHERED;
+        } else if (TextUtils.equals(apnType, PhoneConstants.APN_TYPE_SUPL)) {
+            return (RILConstants.DATA_PROFILE_SUPL);
+        } else if (TextUtils.equals(apnType, PhoneConstants.APN_TYPE_HIPRI)) {
+            return (RILConstants.DATA_PROFILE_HIPRI);
         } else {
             return RILConstants.DATA_PROFILE_DEFAULT;
         }
