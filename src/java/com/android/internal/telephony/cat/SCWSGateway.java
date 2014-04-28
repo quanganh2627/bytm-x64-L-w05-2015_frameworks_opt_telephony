9eef1a35a83160b4f183df9cbbb357658164603e
Andre Amegah <andrex.amegah@intel.com>
2014-04-28 10:35:59 +0200

    BIP: Fix Open channel UICC in Server mode issue.
    BZ: 195877
    
    All Bip UICC Server mode test cases failed due to many reasons.
    Following are main reasons:
    
    - Even though Open Channel command succeed, an error case
      Terminal Response  was sent to UICC that leads to test failed.
    - Close channel states were mishandled.
    - Get channel status and event didn't send proper status to UICC.
    - UICC server mode channel status states (listen, established, closed)
      change depending on some specific events that leads to a
      state machine that was not correctly handled and tests failed.
    - Reading data was not handled in the loop that leads to only one-shot
      data availability sent to ICC.
    - Data available event is sent to UICC even though
      the UICC hasn't read already received data in the
      buffer.
    
      Changes has been done to address previous issues:
    - Properly implemented open channel command.
    - Properly handled channel status state machine.
    - Added a Handler to manage asynchronous messages.
    - Handled client accept by the server in a thread different from the one use
      to handle actual client connection.
    - Reading data was handled in the loop and data available event is sent again
      only after the previously read data is downloaded to UICC.
    
    Change-Id: I2bef4806681c979b4d966142411d5fa7e1e107c2
    Category: device enablement
    Domain: Telephony-Common
    Origin: internal
    Upstream-Candidate: no, proprietary
    Signed-off-by: Andre Amegah <andrex.amegah@intel.com>

--- src/java/com/android/internal/telephony/cat/SCWSGateway.java
+++ src/java/com/android/internal/telephony/cat/SCWSGateway.java
@@ -1,121 +1,197 @@
+
 package com.android.internal.telephony.cat;
 
+import android.os.Handler;
+import android.os.Message;
 import com.android.internal.telephony.cat.BipGateWay.BipTransport;
 import com.android.internal.telephony.cat.CatCmdMessage.ChannelSettings;
 import com.android.internal.telephony.cat.CatCmdMessage.DataSettings;
 
+import java.io.InputStream;
 import java.io.IOException;
 import java.lang.NullPointerException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 class SCWSGateway {
-    private ServerThread mThread;
-    private ChannelSettings mChannelSettings;
-    private int mChannelStatus;
-
-    private ServerSocket mServerSocket;
+    private volatile ChannelSettings mChannelSettings;
+    private volatile int mChannelStatus;
+    private volatile ServerSocket mServerSocket;
     private Socket mSocket;
-
-    private byte[] mRxBuf = new byte[BipGateWay.TCP_CHANNEL_BUFFER_SIZE];
+    private volatile int mCloseQualifierMsg = 0;
+    private byte[] mRxBuf = new byte[BipGateWay.PREFERRED_BUFFER_SIZE];
     private int mRxPos = 0;
     private int mRxLen = 0;
 
-    private byte[] mTxBuf = new byte[BipGateWay.TCP_CHANNEL_BUFFER_SIZE];
+    private byte[] mTxBuf = new byte[BipGateWay.PREFERRED_BUFFER_SIZE];
     private int mTxPos = 0;
     private int mTxLen = 0;
 
+    /* UICC Server mode channel status states
+     * Channel status value is coded in 2 bytes (see ETSI TS 102 223 section 8.56)
+     * The MSB holds actual STATE + channel identifier.
+     * The LSB holds additional information.
+     * The following first three definitions represent possible value of the MSB without
+     * channel identifier.
+     * The last two definitions represent possible values of LSB (addition info).
+     * Note that to ease the actual channel status calculation we use 2 bytes integer to
+     * code each state relative information.
+     */
+    public static final int TCP_IN_CLOSED_STATE       = 0x0000;
+    public static final int TCP_IN_LISTEN_STATE       = 0x4000;
+    public static final int TCP_IN_ESTABLISHED_STATE  = 0x8000;
+    public static final int TCP_STATE_LINK_DROPPED    = 0x0005;
+    public static final int TCP_STATE_NO_FURTHER_INFO = 0x0000;
+
+    static final int MSG_ID_UICC_SERVER_CLOSE_LISTEN_STATE           = 1;
+    static final int MSG_ID_UICC_SERVER_CLOSE_CLOSE_STATE            = 2;
+    static final int MSG_ID_UICC_SERVER_ESTABLISHED_TO_LISTEN_STATE  = 3;
+
     private CatService mCatService = null;
+
+    private final Object mLock = new Object();
+
+    Handler mServerHandler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            CatLog.d(this, "Handle message " + msg);
+            switch (msg.what) {
+                case MSG_ID_UICC_SERVER_ESTABLISHED_TO_LISTEN_STATE:
+                    mCloseQualifierMsg = 0;
+                    createListenThread(true);
+                    break;
+                case MSG_ID_UICC_SERVER_CLOSE_LISTEN_STATE:
+                    mCloseQualifierMsg = 0;
+                    createListenThread(false);
+                    break;
+                case MSG_ID_UICC_SERVER_CLOSE_CLOSE_STATE:
+                    mCloseQualifierMsg = 0;
+                    closeServerSocket();
+                default:
+                    throw new AssertionError("Unrecognized message: " + msg.what);
+            }
+        }
+    };
 
     public SCWSGateway(CatService mCatService) {
         this.mCatService = mCatService;
     }
 
-    public boolean startProxy (CatCmdMessage cmdMsg) {
+    public boolean openChannel(CatCmdMessage cmdMsg) {
+        CatLog.d(this, "Opening channel... ");
         ResultCode resultCode = ResultCode.OK;
 
         mChannelSettings = cmdMsg.getChannelSettings();
         mChannelStatus = mChannelSettings.channel << 8; // Closed state
 
-        if (mChannelSettings.bufSize > BipGateWay.TCP_CHANNEL_BUFFER_SIZE) {
+        if (mChannelSettings.bufSize > BipGateWay.PREFERRED_BUFFER_SIZE) {
             resultCode = ResultCode.PRFRMD_WITH_MODIFICATION;
-            mChannelSettings.bufSize = BipGateWay.TCP_CHANNEL_BUFFER_SIZE;
-        } else {
-            mChannelSettings.bufSize = BipGateWay.TCP_CHANNEL_BUFFER_SIZE;
+            mChannelSettings.bufSize = BipGateWay.PREFERRED_BUFFER_SIZE;
         }
 
-        if (mChannelSettings.bufSize > 0) {
-            mRxBuf = new byte[mChannelSettings.bufSize];
-            mTxBuf = new byte[mChannelSettings.bufSize];
-        }
-        boolean resultFlag = true;
+        mRxBuf = new byte[mChannelSettings.bufSize];
+        mTxBuf = new byte[mChannelSettings.bufSize];
+
         try {
             mServerSocket = new ServerSocket(mChannelSettings.port);
-            new ServerThread().start();
         } catch(IOException e) {
-            resultFlag = false;
-        }
-        if (resultFlag) {
+            setChannelStatus(TCP_IN_CLOSED_STATE, TCP_STATE_LINK_DROPPED,
+                    mChannelSettings.channel);
             ResponseData resp = new OpenChannelResponseData(
                     mChannelSettings.bufSize, mChannelStatus, mChannelSettings.bearerDescription);
             mCatService.sendTerminalResponse(cmdMsg.mCmdDet, ResultCode.BIP_ERROR,
                     true, 0x00, resp);
-        } else {
-            CatLog.d(this, "Open server socket on port "
-                    + mChannelSettings.port + " for channel " + mChannelSettings.channel);
-            CatLog.d(this, "Server thread start on channel no: " + mChannelSettings.channel);
-
-            // Update channel status to listening before sending TR
-            mChannelStatus = 0x4000 + (mChannelSettings.channel << 8);
-            ResponseData resp = new OpenChannelResponseData(
-                    mChannelSettings.bufSize, mChannelStatus, mChannelSettings.bearerDescription);
-            mCatService.sendTerminalResponse(cmdMsg.mCmdDet, resultCode, false, 0, resp);
+            return false;
         }
-        return resultFlag;
+        CatLog.d(this, "Open channel server socket on port "
+                    + mChannelSettings.port + " for channel "
+                    + mChannelSettings.channel + " Buffer size "
+                    + mChannelSettings.bufSize);
+
+        createListenThread(false);
+
+        // Update channel status to listening before sending TR
+        setChannelStatus(TCP_IN_LISTEN_STATE, TCP_STATE_NO_FURTHER_INFO,
+                mChannelSettings.channel);
+        ResponseData resp = new OpenChannelResponseData(
+                mChannelSettings.bufSize, mChannelStatus, mChannelSettings.bearerDescription);
+        mCatService.sendTerminalResponse(cmdMsg.mCmdDet, resultCode, false, 0, resp);
+
+        return true;
     }
 
-    public boolean closeClientConnection(CatCmdMessage cmdMsg) {
-        boolean resultFlag = true;
+    private synchronized void setChannelStatus(int state, int info, int channel) {
+        mChannelStatus = (state + info) + (channel << 8);
+    }
+
+    public int getStatus() {
+        return mChannelStatus;
+    }
+
+    private void initRxAndTxData() {
+        mRxBuf = new byte[mChannelSettings.bufSize];
+        mTxBuf = new byte[mChannelSettings.bufSize];
+        mRxPos = 0;
+        mRxLen = 0;
+        mTxPos = 0;
+        mTxLen = 0;
+    }
+
+    private void closeServerSocket() {
+
+        if (mServerSocket != null && !mServerSocket.isClosed()) {
+             try {
+                 mServerSocket.close();
+             } catch (IOException e) {
+                 CatLog.d(this, "Server Socket is null or already closed");
+             }
+             mServerSocket = null;
+         }
+    }
+
+    private void closeClientSocket() {
+        if (mSocket != null && !mSocket.isClosed()) {
+            try {
+                mSocket.close();
+            } catch (IOException e) {
+                 CatLog.d(this, "Socket is null or already closed");
+            }
+            mSocket = null;
+         }
+    }
+
+    private int getCurrentState() {
+        return mChannelStatus & 0xC000;
+    }
+
+    public void closeChannel(CatCmdMessage cmdMsg) {
+
+        CatLog.d(this, "Closing Channel : " + mChannelSettings.channel
+                + "in state: " + mChannelStatus);
 
         if ((cmdMsg.getCommandQualifier() & 0x01) == 0x01) {
-            if (mSocket != null && !mSocket.isClosed()) {
-                try {
-                    mSocket.close();
-                } catch (IOException e) {
-                    resultFlag = false;
-                }
+            // Close Channel, "TCP in LISTEN state" case
+
+            if (getCurrentState() == TCP_IN_LISTEN_STATE) {
+                // TR to be sent
+            } else if (getCurrentState() == TCP_IN_ESTABLISHED_STATE) {
+                mCloseQualifierMsg = MSG_ID_UICC_SERVER_CLOSE_LISTEN_STATE;
+                closeClientSocket();
             }
-            mSocket = null;
+        } else if ((cmdMsg.getCommandQualifier() & 0x00) == 0x00) {
+            // Close Channel, "TCP in CLOSED state" case
+            if (getCurrentState() == TCP_IN_LISTEN_STATE) {
+                closeServerSocket();
+            } else if (getCurrentState() == TCP_IN_ESTABLISHED_STATE) {
+                mCloseQualifierMsg = MSG_ID_UICC_SERVER_CLOSE_CLOSE_STATE;
+                closeClientSocket();
+            }
         } else {
-            if (mSocket != null && !mSocket.isClosed()) {
-                try {
-                    mSocket.close();
-                } catch (IOException e) {
-                    CatLog.d(this, "Socket is null or already closed");
-                }
-            }
-            mSocket = null;
-
-            if (mServerSocket != null && !mServerSocket.isClosed()) {
-                try {
-                    mServerSocket.close();
-                } catch (IOException e) {
-                    resultFlag = false;
-                }
-            }
-            mServerSocket = null;
-
-            mRxPos = 0;
-            mRxLen = 0;
-            mTxPos = 0;
-            mTxLen = 0;
-            mChannelStatus = mChannelSettings.channel << 8;
-            BipGateWay.getBipTransport().sendChannelStatusEvent(mChannelStatus);
-
-            mCatService.sendTerminalResponse(cmdMsg.mCmdDet, ResultCode.OK, false, 0, null);
-            return true;
+            CatLog.d(this, "No supported Command Qualifier: " + cmdMsg.getCommandQualifier());
         }
-        return resultFlag;
+        mCatService.sendTerminalResponse(cmdMsg.mCmdDet, ResultCode.OK, false, 0, null);
     }
 
     public void send(CatCmdMessage cmdMsg) {
@@ -208,95 +284,120 @@ class SCWSGateway {
 
         resp = new ReceiveDataResponseData(data, available);
         mCatService.sendTerminalResponse(cmdMsg.mCmdDet, resultCode, false, 0, resp);
-    }
 
-    public void stopProxy() {
-        // close any existing client connection
-        // so that we can handle the next waiting client request.
-        if (mSocket != null) {
-            if (!mSocket.isClosed()) {
-                try {
-                    mSocket.close();
-                } catch (IOException ioex) {
-                    // nothing to do, since we don't need this socket
-                    // any longer.
-                    CatLog.d(this, "Socket is null or already closed");
-                }
+        if (mRxLen == 0) {
+            synchronized (mLock) {
+                mLock.notify();
             }
-            mSocket=null;
-        }
-
-        // restart server thread.
-        if (mThread == null || !mThread.isAlive()) {
-            mThread = new ServerThread();
-            mThread.start();
         }
     }
 
-    class ServerThread extends Thread {
+    private void sendMessage(int msgType) {
+        Message msg = mServerHandler.obtainMessage(msgType);
+        mServerHandler.sendMessage(msg);
+    }
+
+    private void createListenThread(boolean isSendEvent) {
+        new ListenThread().start();
+        if (isSendEvent) {
+            sendEventStatus(TCP_IN_LISTEN_STATE,
+                    TCP_STATE_LINK_DROPPED, mChannelSettings.channel);
+        }
+    }
+
+    private void sendEventStatus(int state, int info, int channel) {
+        setChannelStatus(state, info, channel);
+        BipGateWay.getBipTransport().sendChannelStatusEvent(mChannelStatus);
+    }
+
+    // Handle accepting connection
+    class ListenThread extends Thread {
+
         @Override
         public void run() {
-            CatLog.d(this, "Server thread start on channel no: "
-                    + mChannelSettings.channel);
 
-            if (mSocket == null || mSocket.isClosed()) {
-
-                // event download - channel listen
-                mChannelStatus = 0x4000 + (mChannelSettings.channel << 8);
-                BipGateWay.getBipTransport().sendChannelStatusEvent(mChannelStatus);
-
-                try {
-                    CatLog.d(this, "Wait for connection");
-                    mSocket = mServerSocket.accept();
-                } catch (IOException e) {
-                    CatLog.d(this, "IOException " + e.getMessage());
-                    // TODO: find out if serverSocket is OK else we will end up in a loop
-                }
-
-                // event download, channel established
-                if (mSocket != null && mSocket.isConnected()) {
-                    mChannelStatus = 0x8000 + (mChannelSettings.channel << 8);
-                    BipGateWay.getBipTransport().sendChannelStatusEvent(mChannelStatus);
-                }
+            try {
+                mSocket = mServerSocket.accept();
+                new ClientThread().start();
+                sendEventStatus(TCP_IN_ESTABLISHED_STATE,
+                        TCP_STATE_NO_FURTHER_INFO, mChannelSettings.channel);
+            } catch(IOException ie) {
+                CatLog.d(this, "IOException while accepting" + ie.getMessage());
+                closeServerSocket();
+                // TODO handling properly exception
             }
+        }
+    }
 
-            if (mSocket != null) {
-                // client connected, wait until some data is ready
-                try {
-                    mRxLen = mSocket.getInputStream().read(mRxBuf);
-                } catch (IOException e) {
-                    CatLog.d(this, "Read on No: " + mChannelSettings.channel +
-                            ", IOException " + e.getMessage());
-                    mSocket = null; // throw client socket away.
-                    mRxBuf = new byte[mChannelSettings.bufSize];
-                    mTxBuf = new byte[mChannelSettings.bufSize];
-                    mRxPos = 0;
-                    mRxLen = 0;
-                    mTxPos = 0;
-                    mTxLen = 0;
-                }
+    // Handling client connection
+    class ClientThread extends Thread {
 
-                // sanity check
-                if (mRxLen <= 0) {
-                    CatLog.d(this, "No data read.");
-                } else {
+        @Override
+        public void run() {
 
-                    mRxPos = 0;
-                    int available = 0xff;
-                    if (mRxLen < available) {
-                        available = mRxLen;
+            CatLog.d(this, "New Client connection for channel: #"
+                    + mChannelSettings.channel
+                    + " on port: #"
+                    + mChannelSettings.port
+                    + " at socket: "
+                    + mSocket);
+            initRxAndTxData();
+            InputStream inStream = null;
+
+            try {
+                inStream = mSocket.getInputStream();
+
+                while (mSocket != null && !mSocket.isClosed()) {
+                    // client connected, wait until some data is ready
+                    mRxLen = inStream.read(mRxBuf);
+
+                    // sanity check
+                    if (mRxLen <= 0) {
+                        CatLog.d(this, "No data read.");
+                        sendMessage(MSG_ID_UICC_SERVER_ESTABLISHED_TO_LISTEN_STATE);
+                        break;
+                    } else {
+
+                        mRxPos = 0;
+                        int available = 0xff;
+                        if (mRxLen < available) {
+                            available = mRxLen;
+                        }
+
+                        // event download, data available
+                        BipGateWay.getBipTransport().sendDataAvailableEvent(
+                                mChannelStatus, (byte) (available & 0xff));
                     }
 
-                    // event download, data available
-                    BipGateWay.getBipTransport().sendDataAvailableEvent(
-                            mChannelStatus, (byte) (available & 0xff));
+                    synchronized (mLock) {
+                        try {
+                            mLock.wait();
+                        } catch (InterruptedException e) {
+                            CatLog.d(this, "Exception in waiting for data read notification");
+                        }
+                    }
                 }
-            } else {
-                CatLog.d(this, "No Socket connection for server thread on channel no: "
-                        + mChannelSettings.channel);
+            } catch (IOException e) {
+                CatLog.d(this, "Read on No: " + mChannelSettings.channel +
+                        ", IOException " + e.getMessage());
+                if (mCloseQualifierMsg != 0) {
+                    sendMessage(mCloseQualifierMsg);
+                } else {
+                 sendMessage(MSG_ID_UICC_SERVER_ESTABLISHED_TO_LISTEN_STATE);
+                }
+            } finally {
+                synchronized (mLock) {
+                    mLock.notify();
+                }
+                closeClientSocket();
+                try {
+                    if (inStream != null) {
+                        inStream.close();
+                    }
+                } catch(IOException ie) {
+                    CatLog.d(this, "IOException " + ie.getMessage());
+                }
             }
-
-            CatLog.d(this, "Server thread end on channel no: " + mChannelSettings.channel);
         }
     }
 }
